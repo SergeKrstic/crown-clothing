e7e06a3ab88438c2f33a22811de3551dc67d531e
Serge Krstic <srdjankrstic@gmail.com>
2019-08-11 21:57:21 +0800

    Separating our cra app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
-const onToken = token => {
-  console.log(token);
-  alert('Payment Successful');
-};
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceFromStripe = price * 100;
+  const priceForStripe = price * 100;
   const publishableKey = 'pk_test_PjyvybHGdSXRDVydkLkQ9iOt00BOXp7hp2';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please ensure you use the provided credit card.'
+        );
+      });
+  };
 
   return (
     <StripeCheckout
@@ -18,7 +34,7 @@ const StripeCheckoutButton = ({ price }) => {
       shippingAddress
       image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
-      amount={priceFromStripe}
+      amount={priceForStripe}
       panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
